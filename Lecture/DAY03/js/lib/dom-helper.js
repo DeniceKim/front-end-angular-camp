f00e958c001b49e28243744d01bc0dcd083f4bcc
Jee Hoon <yamoo9@naver.com>
2016-03-10 11:06:26 +0900

    type() 함수를 dom-helper.js 로 이동

--- Lecture/DAY03/js/lib/dom-helper.js
+++ Lecture/DAY03/js/lib/dom-helper.js
@@ -74,3 +74,37 @@ function each(data, callback) {
     }
 
 }
+
+/**
+ * Javascript 데이터 유형을 감지하여 올바르게 값을 반환하는 함수
+ * @param  {all_data_format} data 모든 데이터 유형이 전달될 수 있음
+ * @return {String}      데이터 유형 이름을 문자열로 반환
+ */
+function type(data) {
+    // Object.prototype.toString 메소드 빌려쓰기
+    return Object.prototype.toString.call(data).toLowerCase().slice(8,-1);
+}
+// type 함수를 재사용하여 결과 값을 true | false로 설정
+function isNumber(data) {
+    return type(data) === 'number';
+}
+
+function isString(data) {
+    return type(data) === 'string';
+}
+
+function isBoolean(data) {
+    return type(data) === 'boolean';
+}
+
+function isFunction(data) {
+    return type(data) === 'function';
+}
+
+function isArray(data) {
+    return type(data) === 'array';
+}
+
+function isObject(data) {
+    return type(data) === 'object';
+}
