6a916028638992db8b32886c4254ab216b606a28
Jee Hoon <yamoo9@naver.com>
2016-03-10 10:49:24 +0900

    안티패턴 5 - 잘못 설계된 typeof, instanceof와 객체가 아닌 것을 판별할 수 없는 constructor 사용에 주의하라.
    typeof 설계 오류
    instanceof 설계 오류
    constructor
    
    type() 헬퍼함수를 제작해야 하는 이유

--- Lecture/DAY03/js/anti-patterns.js
+++ Lecture/DAY03/js/anti-patterns.js
@@ -56,3 +56,65 @@ var num = 9,
     obj = {},
     // new RegExp('[0-9][a-z]', 'ig');
     reg = /[0-9][a-z]/ig;
+
+
+
+// -------------------------------------------------------------------------------------------
+// 안티패턴 5번째: 잘못 설계된 typeof, instanceof와 객체가 아닌 것을 판별할 수 없는 constructor 사용에 주의하라.
+// -------------------------------------------------------------------------------------------
+
+/**
+ * ------------------------------------------------
+ * typeof 설계 오류
+ * null, Array 모두 'object'로 값을 반환한다.
+ * --------------------------------------------- */
+console.log( typeof null ); // 'object'
+console.log( typeof [] );   // 'object'
+
+/**
+ * ------------------------------------------------------------------
+ * instanceof 설계 오류
+ * 인스턴스 객체 instanceof 생성자 함수
+ *
+ * typeof의 설계 문제 중 일부 해결 가능
+ * 배열 인스턴스를 배열 생성자의 인스턴스라고 올바르게 이야기 해준다.
+ *
+ * 하지만...
+ * 배열 인스턴스를 객체 생성자의 인스턴스라고도 말한다. (오류)
+ * 원시 데이터 유형(숫자, 문자, 불린)의 경우는 리터럴 사용 시 잘못된 결과를 반환한다.
+ * --------------------------------------------------------------- */
+console.log( [] instanceof Array );  // true
+console.log( [] instanceof Object ); // true
+
+console.log( 'fastcampus' instanceof String );             // false
+console.log( new String('fastcampus') instanceof String ); // true
+
+/**
+ * ------------------------------------------------------------------------------
+ * constructor
+ * 인스턴스 객체는 constructor 속성으로 자신을 생성한 생성자를 참조한다.
+ * (prototype 객체에 링크된 인스턴스 객체이므로 prototype 객체의 속성을 모두 사용 가능하기 때문)
+ *
+ * Javascript 모든 객체 유형에서는 올바르게 객체의 생성자를 말해준다.
+ * 하지만... 객체가 아닌 유형(null, undefined)에서는 오류(TypeError)를 발생한다.
+ * --------------------------------------------------------------------------- */
+console.log( (9).constructor === Number              ); // true
+console.log( ([]).constructor === Object             ); // false
+console.log( (function(){}).constructor === Function ); // true
+console.log( ({}).constructor === Array              ); // false
+
+// null, undefined는 객체가 아니기 때문에 constructor 속성 값이 없어 오류 발생
+console.log( (null).constructor      ); // Uncaught TypeError: Cannot read property 'constructor' of null
+console.log( (undefined).constructor ); // Uncaught TypeError: Cannot read property 'constructor' of undefined
+
+
+/**
+ * --------------------------------------------------
+ * type()
+ * 객체/비객체 모두 판별 가능한 헬퍼 함수 제작이 필요한 이유이다.
+ * ----------------------------------------------- */
+
+// type() 헬퍼함수 사용 예시
+// type(9);    // 'number'
+// type(null); // 'null'
+// type([]);   // 'array'
