175a5c2a706a1478c0146cd81741bb72ae2de1bf
Jee Hoon <yamoo9@naver.com>
2016-03-10 11:03:00 +0900

    type() 헬퍼 함수 - 제작 다른 방법
    Object.prototype.toString 메소드 빌려쓰기를 통해 객체/비객체를 판별하는 방법

--- Lecture/DAY03/js/anti-patterns.js
+++ Lecture/DAY03/js/anti-patterns.js
@@ -147,3 +147,46 @@ function type(data) {
         }
     }
 }
+
+
+/**
+ * --------------------------------------------------
+ * type() 헬퍼 함수 - 제작 다른 방법
+ * Object.prototype.toString 메소드 빌려쓰기를 통해
+ * 객체/비객체를 판별하는 방법
+ * ----------------------------------------------- */
+
+/**
+ * Javascript 데이터 유형을 감지하여 올바르게 값을 반환하는 함수
+ * @param  {anything} data  모든 데이터 유형이 전달될 수 있음
+ * @return {string}         데이터 유형 이름을 문자열로 반환
+ */
+function type(data) {
+    // Object.prototype.toString 메소드 빌려쓰기
+    // [Object Number], [Object Null]... 문자열 반환
+    return Object.prototype.toString.call(data).toLowerCase().slice(8,-1);
+}
+// type 함수를 재사용하여 결과 값을 true | false로 설정
+function isNumber(data) {
+    return type(data) === 'number';
+}
+
+function isString(data) {
+    return type(data) === 'string';
+}
+
+function isBoolean(data) {
+    return type(data) === 'boolean';
+}
+
+function isFunction(data) {
+    return type(data) === 'function';
+}
+
+function isArray(data) {
+    return type(data) === 'array';
+}
+
+function isObject(data) {
+    return type(data) === 'object';
+}
