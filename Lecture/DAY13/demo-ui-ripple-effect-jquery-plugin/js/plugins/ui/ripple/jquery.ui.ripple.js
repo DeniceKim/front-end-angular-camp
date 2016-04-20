21b4571391bfaf38153de9f73cc9cd0c759471b3
Jee Hoon <yamoo9@naver.com>
2016-04-20 22:19:38 +0900

    리플 이펙트 구현 코드 완성

--- Lecture/DAY13/demo-ui-ripple-effect-jquery-plugin/js/plugins/ui/ripple/jquery.ui.ripple.js
+++ Lecture/DAY13/demo-ui-ripple-effect-jquery-plugin/js/plugins/ui/ripple/jquery.ui.ripple.js
@@ -58,6 +58,9 @@
 			// 방법 2. 부모 요소에 식별자 클래스 속성 추가
 			$ripple_parent.addClass('yamoo9-ui-ripple-parent');
 
+			// 리플 요소에 식별자 클래스 속성 추가
+			$ripple.addClass('yamoo9-ui-ripple');
+
 			// 부모 $ripple_parent의 가로/세로 중 긴 길이 값을 dimension 변수에 참조
 			dimension = Math.max($ripple_parent.outerWidth(), $ripple_parent.outerHeight());
 
@@ -88,7 +91,7 @@
 
 			// 이벤트 핸들러 정의
 			function activeRipple(e) {
-				var x, y;
+				var x, y, parent_offset, r, $ink;
 				e.stopPropagation(); // 이벤트 전파 멈춤
 				e.preventDefault(); // 기본 동작 차단
 				// this는 무엇을 참조하나?
@@ -97,13 +100,35 @@
 				// 팩토리를 사용하면 성능 저하.. 우려
 				// 네이티브 자바스크립트를 사용하거나...
 				// 다른 방법을 모색해야 한다.
-				console.log('clicked $ripple', this);
+				// console.log('clicked $ripple', this);
 
 				// 마우스 좌표 x, y 값을 확인
-				console.log('e.pageX:', e.pageX, 'e.pageY:', e.pageY);
-				x = e.pageX - this.parent().offset().left;
-				y = e.pageY - this.parent().offset().top;
-				console.log('x:', x, 'y:', y);
+				// console.log('e.pageX:', e.pageX, 'e.pageY:', e.pageY);
+				parent_offset = this.parent().offset();
+				r = dimension/2;
+				// 사용자가 마우스로 클릭한 위치를 구하는 공식
+				// 페이지에서 마우스로 클릭한 위치 - 클릭된 a의 부모의 페이지에서의 위치 - 리플 요소의 지름/2
+				x = e.pageX - parent_offset.left - r;
+				y = e.pageY - parent_offset.top - r;
+
+				// 클릭된 a 요소의 다음 $ink 요소를 변수에 참조
+				var $ink = this.next();
+
+				// 사용자가 a를 클릭했을 때, $ink에 animate 클래스 속성 제거
+				$ink.removeClass('animate');
+
+				// $ink의 top, left 위치를 x, y 축으로 설정한다.
+				$ink
+					.hide()
+					.css({
+						'left': x,
+						'top': y,
+					})
+					.show();
+
+				// 사용자가 a를 클릭했을 때, $ink에 animate 클래스 속성 추가
+				$ink.addClass('animate');
+
 			}
 
 		}
@@ -115,6 +140,7 @@
 		'prefix': 'yamoo9',
 		'containerClass': 'ui-ripple-container',
 		'parentClass': 'ui-ripple-parent',
+		'rippleClass': 'ui-ripple',
 		'inkClass': 'ui-ripple-ink',
 		'color': '#7045CF'
 	};
