69ec7b69b6b90a5d98916afd1ef1173b4990467f
Jee Hoon <yamoo9@naver.com>
2016-04-30 10:54:45 +0900

    첫시간 진행 내용 정리

--- Lecture/DAY16/demo-gulpJS/gulpfile.js
+++ Lecture/DAY16/demo-gulpJS/gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
 // 의존 모듈 로드
-var gulp       = require('gulp');
-var yargs      = require('yargs').argv;
-var log        = require('./gulp_tasks/utils/log');
-var $          = require('gulp-load-plugins')({'lazy': true});
+var gulp  = require('gulp');
+var yargs = require('yargs').argv;
+var log   = require('./gulp_tasks/utils/log');
+var $     = require('gulp-load-plugins')({'lazy': true});
 
 // --------------------------------------------------
 // 기본 업무 등록
 // gulp.task('default', []);
 gulp.task('default', ()=> {
-  log({
-    'first': 'Gulp 업무 시작!!!',
-    'second': '테스트 중....'
-  }, 'red');
+  // 1. log() 함수의 첫번째 인자가 문자인 경우
+  log('gulp 업무 시작');
+  // 2. log() 함수의 첫번째 인자가 객체인 경우
+  // log({
+  //   'first': 'Gulp 업무 시작!!!',
+  //   'second': '테스트 중....'
+  // }, 'red');
 });
 
