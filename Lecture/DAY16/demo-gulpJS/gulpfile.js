81ff6dee7152d7a204668af1e5cbfe258ceb4051
Jee Hoon <yamoo9@naver.com>
2016-04-30 12:04:56 +0900

    3번째 수업 시작 전 코드 동기화

--- Lecture/DAY16/demo-gulpJS/gulpfile.js
+++ Lecture/DAY16/demo-gulpJS/gulpfile.js
@@ -4,7 +4,12 @@
 var gulp  = require('gulp');
 var yargs = require('yargs').argv;
 var log   = require('./gulp_tasks/utils/log');
-var $     = require('gulp-load-plugins')({'lazy': true});
+// Browserify 사용을 위한 모듈 로드
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+// Gulp 플러그인 모듈 로더
+var $ = require('gulp-load-plugins')({'lazy': true});
 
 // --------------------------------------------------
 // 기본 업무 등록
@@ -19,3 +24,24 @@ gulp.task('default', ()=> {
   // }, 'red');
 });
 
+// --------------------------------------------------
+// 번들링(Bundling): Javascript 업무 등록
+// --------------------------------------------------
+gulp.task('bundle:js', ()=> {
+  // 번들러 옵션 설정
+  var bundler = browserify({
+    // 진입 파일 (번들링 시작 파일)
+    'entries': './test/app.js',
+    // 소스맵을 번들링된 파일에 포함
+    'debug': false,
+    // 번들링 속도 향상 (다만 파일 크기가 커짐)
+    'insertGlobals': true
+  });
+
+  return bundler
+    .bundle()
+    .pipe(source('app.bundle.js'))
+    .pipe(buffer())
+    .on('error', $.util.log)
+    .pipe(gulp.dest('./.tmp'));
+});
