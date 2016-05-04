2e6839dc7ff41ac12cc1acdf7d0326037ae05abd
Jee Hoon <yamoo9@naver.com>
2016-05-04 18:46:54 +0900

    17일차 강의 진행 준비

--- Lecture/DAY16/demo-gulpJS/gulpfile.js
+++ Lecture/DAY16/demo-gulpJS/gulpfile.js
@@ -1,35 +1,43 @@
 'use strict';
 
+// --------------------------------------------------
 // 의존 모듈 로드
+// --------------------------------------------------
 var config = require('./gulp.config');
 var gulp   = require('gulp');
 var yargs  = require('yargs').argv;
 var log    = require('./gulp_tasks/utils/log');
+// --------------------------------------------------
 // Browserify 사용을 위한 모듈 로드
-// Watchify, lodash.assign ($.extend())
+// Watchify, lodash.assign
+// --------------------------------------------------
 var browserify = require('browserify');
 var watchify   = require('watchify');
 var assign     = require('lodash.assign');
 var source     = require('vinyl-source-stream');
 var buffer     = require('vinyl-buffer');
+// --------------------------------------------------
 // Gulp 플러그인 모듈 로더
+// --------------------------------------------------
 var $ = require('gulp-load-plugins')({'lazy': true});
+
+
 
 // --------------------------------------------------
 // 기본 업무 등록
-gulp.task('default', ['bundle:js']);
+// --------------------------------------------------
+gulp.task('default', ['bundle:js', 'sass:watch']);
+
+
 
 // --------------------------------------------------
 // 번들링(Bundling): Javascript 업무 등록
 // --------------------------------------------------
-// 옵션 덮어쓰기
-var opts = assign({}, watchify.args, config.browserify.options);
-
-// Watchify 래핑된 Browserify 객체
-var bundler = watchify(browserify(opts));
-
+// 번들러 정의
+var bundler = browserify(config.browserify.options);
 // Bundle 업무를 수행하는 함수
-var bundleHandler = () => {
+var bundleHandler = (message) => {
+  log(message);
   return bundler
     .bundle()
     .pipe(source(config.browserify.output_filename))
@@ -45,19 +53,27 @@ var bundleHandler = () => {
     .pipe(gulp.dest(config.browserify.output));
 };
 
-// Gulp 업무 등록
-gulp.task('bundle:js', bundleHandler);
-// 이벤트 처리(감지)
-bundler.on('update', bundleHandler);
-bundler.on('log', $.util.log);
+// browserify 업무
+gulp.task('browserify', bundleHandler.bind(gulp, 'Javascript 번들링'));
+
+// browserify 관찰 업무
+gulp.task('browserify:watch', function() {
+  // 옵션 덮어쓰기
+  var opts = assign({}, watchify.args, config.browserify.options);
+  // Watchify 래핑된 Browserify 객체
+  bundler = watchify(browserify(opts));
+  bundleHandler.call(gulp, '번들링 관찰 중...');
+  // 이벤트 처리(감지)
+  bundler.on('update', bundleHandler);
+  bundler.on('log', $.util.log);
+});
+
 
 // --------------------------------------------------
 // 프리프로세싱(Pre-Processing): Sass → CSS 업무 등록
 // --------------------------------------------------
 gulp.task('sass', ()=> {
-
   log('Sass → CSS 변환');
-
   return gulp
     .src('./test/*.s+(a|c)ss')
     // 소스맵 초기화
@@ -89,7 +105,8 @@ gulp.task('sass:lint', ()=> {
     .pipe($.sassLint.failOnError());
 });
 
-gulp.task('sass:watch', ['sass', 'sass:lint'], ()=> {
+gulp.task('sass:watch', ['sass'], ()=> {
+  log('Sass 관찰 업무');
   gulp.watch(['./test/*.{sass,scss}'], ['sass']);
 });
 
