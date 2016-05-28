6823dd5b750d4d8ddc837a20fe311eafdef612c9
Jee Hoon <yamoo9@naver.com>
2016-05-28 16:02:54 +0900

    22일차 수업 정리

--- Lecture/DAY22/project-startkit/src/js/app.js
+++ Lecture/DAY22/project-startkit/src/js/app.js
@@ -21,7 +21,7 @@ angular.module('PersonListApp', [])
     // $scope.selectedPerson 속성의 초기값 설정
     $scope.selectedPerson = null;
     // 사용자가 검색하는 이름/메일을 포함하는 객체 초기 설정
-    $scope.search = {};
+    $scope.search = '';
 
     // Ajax를 사용하여 모델 데이터 로드
     $http
@@ -83,7 +83,19 @@ angular.module('PersonListApp', [])
         case 'TR': return 'Turkey';
         case 'US': return 'United States';
       }
-    }
+    };
+
+    $scope.sensitiveSearch = function(person) {
+      // 초기 로드할 때는 거짓이 나오도록 설정
+      if ( $scope.search ) {
+        return person.name.first.indexOf($scope.search) === 0 ||
+               person.name.last.indexOf($scope.search) === 0 ||
+               person.gender.indexOf($scope.search) === 0 ||
+               person.email.indexOf($scope.search) === 0;
+      }
+      // 처음 로드할 때는 모든 조건이 참.
+      return true;
+    };
 
   }]);
 
